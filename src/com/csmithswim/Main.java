3b631bac76ec314a42bcbd5093c5adbae2a4f38b
Cullen S <cullensmi@gmail.com>
2020-12-08 12:33:10 -0500

    Solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,6 +1,4 @@
 package com.csmithswim;
-
-
 import java.util.*;
 
 abstract class Book {
@@ -16,6 +14,21 @@ abstract void display();
         }
 
 // Declare your class here. Do not use the 'public' access modifier.
+class MyBook extends Book{
+
+    protected int price;
+
+    MyBook(String title, String author, int price){
+        super(title, author);
+        this.title=title;
+        this.author=author;
+        this.price=price;
+    }
+    void display(){
+        System.out.println("Title: "+title+"\nAuthor: "+author+"\nPrice: "+price);
+    }
+
+}
 // Declare the price instance variable
 
 /**
